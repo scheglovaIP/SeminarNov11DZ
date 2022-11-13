b6ff1353b90bbdb8bcfddea4ef9b65ae344a6867
Irina <ip_scheglova@mail.ru>
2022-11-13 23:06:07 +0500

    add Home Work Number 2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1>number2) Console.Write(number1 + " > " + number2);
+else if (number1<number2) Console.Write(number1 + " < " + number2);
+else Console.Write("Числа равны!");
