47210845434705e51112990c860818e2200de846
Irina <ip_scheglova@mail.ru>
2022-11-13 23:15:58 +0500

    add Home Work Number 6

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 == 0) Console.Write("Число " + number + " - четное");
+else Console.Write("Число " + number + " - нечетное");
