89529b62a0497a7f0b5ebd76528f6e15c2b46a87
Irina <ip_scheglova@mail.ru>
2022-11-13 23:34:42 +0500

    add Home Work Number 8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number<=1) Console.Write("Вы ввели число меньше или равное 1");
+else
+{
+    int count = 1;
+while(count<=number)
+{
+    if(count % 2 == 0) Console.Write(count + ", ");
+    count++;
+}
+}
